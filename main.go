2b46a1aa9849bf255814e846d206bbbe254141ba
sai umesh <saiumesh.dhanewar@apty.io>
2021-12-29 15:27:07 +0530

    yeah blamed

--- main.go
+++ main.go
@@ -27,7 +27,6 @@ func runMain() {
 	}
 	segmentIOKey := fmt.Sprintf("SEGMENT_IO_KEY_%s", strings.ToUpper(branch))
 	segmentIOValue := secretsMap[segmentIOKey]
-	fmt.Println("the output is", "HEHEHE "+segmentIOValue)
 	core.SetOutput("SEGMENT_IO_KEY", segmentIOValue)
 }
 
